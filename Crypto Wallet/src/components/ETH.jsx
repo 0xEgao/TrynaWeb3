0662af18206c63245d2c172dee4719f4fe446172
skid-kid <polarb587@gmail.com>
2025-01-05 18:12:30 +0530

    added functionality to chekc ETH balance

--- Crypto Wallet/src/components/ETH.jsx
+++ Crypto Wallet/src/components/ETH.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import axios from "axios";
 import { mnemonicToSeed } from "bip39";
 import { Wallet, HDNodeWallet } from "ethers";
 
 export const EthWallet = ({mnemonic}) => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [addresses, setAddresses] = useState([]);
+    const [balance, setBalance] = useState("");
 
     return (
         <div>
@@ -23,6 +25,22 @@ export const EthWallet = ({mnemonic}) => {
 
             {addresses.map(p => <div className="text-white text-xl">
                 ETH public key {currentIndex}-: {p}
+                <button onClick={async function () {
+                    const response = await axios.post("https://worldchain-mainnet.g.alchemy.com/v2/zEqt3CLpTNouKY7ycqhGZiobMnRtae5O", {
+                            "jsonrpc": "2.0",
+                            "id": 1,
+                            "method": "eth_getBalance",
+                            "params": [p]
+                    })
+                    setBalance(response.data.result);
+                }
+                }  className="bg-blue-500 p-3 text-white rounded-sm  text-center m-3">
+                    Get Balance
+                </button>
+             <h1 className="text-white text-xl">
+                 {parseInt(balance,16)}
+            </h1>
+
             </div>)}
         </div>
     )
